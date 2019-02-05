171f01afd6707c8b89045ed98a273f5710e74c87
Amjadrathod <amjadrathod@gmail.com>
2019-02-05 18:24:21 +0530

    Update all

--- assets/js/main.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-  // var path = window.location.pathname;
-  // $(".flexnav li a[href*='" + path + "']").addClass("active");
-  var path = window.location.pathname;
-  if (path.substr(path.length) != 1) {
-    $(".flexnav li a[href='" + path + "']").addClass("active");
-  }
-});
